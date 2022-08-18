32e4e1465e238d3908b89b49d49b35adde4d150a
Jobsidney <jobsidney67@gmail.com>
2022-08-18 08:49:49 +0300

    commit tree b0c47f2151e679f45ef19b206807426749feb9da

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
-
+    def name=(name)
+        @name = name
+    end
+    def  name
+        @name
+    end
+    def job=(job)
+        @job=job
+    end
+    def job
+        @job
+    end
 end
+
+sid=Person.new
+sid.name="Jovan"
+sid.job="ENgineer"
+p sid.job
